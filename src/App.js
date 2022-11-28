d5159c8a60464700f3c9f8c83194343842e02c5c
yugipoke27 <yugipoke27@gmail.com>
2022-11-28 15:21:33 -0500

    deploy react app to github

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lab 10 Problem 3</h1>
+      <h1>Kevin Shao</h1>
+
+
+    </div>
+  );
+}
+
+export default App;
