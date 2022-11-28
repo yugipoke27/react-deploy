3144a2a8116cccdb0d7953d2a4feb2203c9d03a0
yugipoke27 <yugipoke27@gmail.com>
2022-11-28 15:48:14 -0500

    commit3

--- src/App.js
+++ src/App.js
@@ -9,26 +9,26 @@ function App() {
 
       <p>The Framework I decided to use was React.</p>
       <p>React is a very good Framework that implements Javascript, HTML and CSS all in one folder by downloading an APP.</p>
+    <div class = "center">
+      <div class = "steps">
+        <h2>Steps I took to set up this Framework</h2>
+          <ol>
+            <li>Download Git and Node.js</li>
+            <li>Create the folders needed on the local computer for react website</li>
+            <li>Create new directory on github to store all the code needed for website</li>
+            <li>Edit the package.json file to store the HOMEPAGE</li>
+            <li>Connect the react script with GIT using git commands</li>
+            <li>Push code into the git repository</li>
+            <li>Upload code to the git repository</li>
+            <li>View website: https://yugipoke27.github.io/react-deploy/</li>
+          </ol> 
+      </div>
 
-    <div class = "steps">
-      <h2>Steps I took to set up this Framework</h2>
-        <ol>
-          <li>Download Git and Node.js</li>
-          <li>Create the folders needed on the local computer for react website</li>
-          <li>Create new directory on github to store all the code needed for website</li>
-          <li>Edit the package.json file to store the HOMEPAGE</li>
-          <li>Connect the react script with GIT using git commands</li>
-          <li>Push code into the git repository</li>
-          <li>Upload code to the git repository</li>
-          <li>View website: https://yugipoke27.github.io/react-deploy/</li>
-        </ol> 
     </div>
-
-
       <p>Difficulties Encountered</p>
       <p>I was unable to connect the code with git initially. Was able to fix this error by researching the git commands needed</p>
       <p>Unable to see changes on the webside. Was able to fix this by runnning the program locally before pushing to git</p>
-
+      
       <p>Overall the process of setting up React Framework was very simple and I would reccomend</p>
     </div>
   );
